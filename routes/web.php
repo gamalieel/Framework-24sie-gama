2d5f3d719d79cff27c93398acb9e2ef52be63fd8
gama <gama@pcr.ac.id>
2025-09-24 09:51:26 +0700

    Penambahan Home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 Route::get('/', function () {
     return view('welcome');
@@ -19,4 +20,5 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
 Route::get('/about', function (){return view('halaman-about');
 });
+Route::get('/home', [HomeController::class, 'index']);
 
